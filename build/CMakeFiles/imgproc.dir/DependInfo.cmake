95fd90076147eac500fb0053d40ce77a411b75b6
MattiaManneschi <mattia.manneschi@stud.unifi.it>
2026-02-02 11:29:32 +0100

    Results added

---
+++ build/CMakeFiles/imgproc.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mattia/final_term_kIP_parallel/src/gpu_convolution.cu" "CMakeFiles/imgproc.dir/src/gpu_convolution.cu.o" "gcc" "CMakeFiles/imgproc.dir/src/gpu_convolution.cu.o.d"
+  "/home/mattia/final_term_kIP_parallel/src/advanced_tests.cpp" "CMakeFiles/imgproc.dir/src/advanced_tests.cpp.o" "gcc" "CMakeFiles/imgproc.dir/src/advanced_tests.cpp.o.d"
+  "/home/mattia/final_term_kIP_parallel/src/benchmark.cpp" "CMakeFiles/imgproc.dir/src/benchmark.cpp.o" "gcc" "CMakeFiles/imgproc.dir/src/benchmark.cpp.o.d"
+  "/home/mattia/final_term_kIP_parallel/src/cpu_convolution.cpp" "CMakeFiles/imgproc.dir/src/cpu_convolution.cpp.o" "gcc" "CMakeFiles/imgproc.dir/src/cpu_convolution.cpp.o.d"
+  "/home/mattia/final_term_kIP_parallel/src/image_io.cpp" "CMakeFiles/imgproc.dir/src/image_io.cpp.o" "gcc" "CMakeFiles/imgproc.dir/src/image_io.cpp.o.d"
+  "/home/mattia/final_term_kIP_parallel/src/kernels.cpp" "CMakeFiles/imgproc.dir/src/kernels.cpp.o" "gcc" "CMakeFiles/imgproc.dir/src/kernels.cpp.o.d"
+  "/home/mattia/final_term_kIP_parallel/src/main.cpp" "CMakeFiles/imgproc.dir/src/main.cpp.o" "gcc" "CMakeFiles/imgproc.dir/src/main.cpp.o.d"
+  "/home/mattia/final_term_kIP_parallel/src/utils.cpp" "CMakeFiles/imgproc.dir/src/utils.cpp.o" "gcc" "CMakeFiles/imgproc.dir/src/utils.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
